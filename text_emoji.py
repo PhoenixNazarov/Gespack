68f001fe64b4ee4dc99cd6c6482d868085825a8e
vanerk03 <vanerk4@gmail.com>
2022-10-01 16:32:50 +0300

    add tts translator

--- text_emoji.py
+++ text_emoji.py
@@ -1,44 +1,29 @@
-from strenum import StrEnum
+from dataclasses import dataclass
 import emoji
 import requests
 import text2emotion as te
-import nltk
+from tot_hacaton.sound import text_to_speech
 
-
+@dataclass
 class Message:
-    def __init__(self, text: str, sound: int = 0) -> None:
-        self.emotion = message_to_code(text)
-        self.is_voice = False
-        self.voice = message_to_sound(text, sound)
+    def __init__(self, text: str, sound:int=-1) -> None:
+        self.emotion = message_to_code(text)  
+        self.voice = text_to_speech(text, sound, "test.wav") if sound != -1 else None
         self.text = text
-
+        self.id = 0
+    
 
 def message_to_code(text: str):
     emotions = te.get_emotion(text)
-    emotion = max(emotions.keys(), key = lambda x: emotions[x])
+    emotion = max(emotions.keys(), key=lambda x: emotions[x])  
     match emotion:
-        case "Happy":
-            return "#21ff34"
-        case "Angry":
-            return "#fa020b"
-        case "Surprise":
-            return "#f7ef4d"
-        case "Sad":
-            return "#74cdfc"
-        case "Fear":
-            return "#ffa621"
+        case "Happy" : return "#21ff34"
+        case "Angry" : return "#fa020b"
+        case "Surprise" : return "#f7ef4d"
+        case "Sad" : return "#74cdfc"
+        case "Fear" : return "#ffa621"
+        case _: return "#74cdfc"
 
-
-def message_to_sound(text: str, flag: int):
-    if flag == 0:
-        return None
-    return
-
-
-sounds = {
-    0: "NO_SOUND",
-    1: "Arthas"
-}
 
 if __name__ == "__main__":
     print(message_to_code('Good'))
