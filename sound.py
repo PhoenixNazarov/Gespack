4e1c671f1b72b085d6b283d1b6f349612c849e1c
vanerk03 <vanerk4@gmail.com>
2022-10-01 16:32:50 +0300

    add tts translator

---
+++ sound.py
@@ -0,0 +1,32 @@
+from importlib.resources import path
+import os
+import torch
+
+
+def text_to_speech(text: str, path: str = "audio.wav", code: int = 0):
+    # TODO add different sound acting based on the code
+    device = torch.device('cpu')
+    torch.set_num_threads(4)
+    local_file = './models/model1.pt'
+
+    if not os.path.isfile(local_file):
+        torch.hub.download_url_to_file('https://models.silero.ai/models/tts/en/v3_en.pt',
+                                       local_file)
+
+    model = torch.package.PackageImporter(
+        local_file).load_pickle("tts_models", "model")
+    model.to(device)
+    sample_rate = 48000
+    
+    if code >= len(speakers):
+        raise ValueError("Invalid code parameter")
+
+    speakers = ['en_0']
+    model.save_wav(audio_path=path,
+                   text=text,
+                   speaker=speakers[code],
+                   sample_rate=sample_rate)
+
+
+if __name__ == "__main__":
+    text_to_speech("lol")
