ab71d5ba1c8510c3ff5a678f7fe0e0e7e60c9cf9
Владимир Назаров <vova24848@gmail.com>
2022-10-01 23:08:44 +0300

    0.4.1 gyro fix

--- static/shake.js
+++ static/shake.js
@@ -19,14 +19,13 @@ function check_pos(event, a, b, y) {
     r2 = mx - mn;
     r_b = Math.min(r1, r2);
 
-    mn = Math.min(event.beta, y) + 90;
-    mx = Math.max(event.beta, y) + 90;
+    mn = Math.min(event.gamma, y) + 90;
+    mx = Math.max(event.gamma, y) + 90;
     r1 = mn + (179 - mx);
     r2 = mx - mn;
     r_y = Math.min(r1, r2);
 
-    console.log(r_a + " " + r_b + " " + r_y);
-    return r_a <= eps_a && r_b <= aps_b && r_y <= eps_y;
+    return (r_a <= eps_a || a === -1) && (r_b <= aps_b || b === -1) && (r_y <= eps_y || y === -1);
 }
 
 var outputRed = document.querySelector('.outputRed');
@@ -39,11 +38,11 @@ window.addEventListener('deviceorientation', (event) => {
     outputGreen.innerHTML = Math.floor(event.gamma);
     outputBlue.innerHTML = Math.floor(event.beta);
 
-    if (check_pos(event, 0, 90, 0)) {
+    if (check_pos(event, -1, 90, -1)) {
         last_norm = Date.now();
         console.log("ys");
     }
- else if (check_pos(event, -90, 179, 0)) {
+ else if (check_pos(event, -1, 179, 0)) {
         if ( Date.now() - last_norm <= 1000) {
             outputPos.innerHTML = 'YEEEEEEES';
             setTimeout(() => outputPos.innerHTML = '', 3000);
